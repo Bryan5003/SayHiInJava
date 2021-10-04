a6f0ca909baea42eabd243eca7c4395cdd6074b3
Bryan5003 <bryanrampersaud@gmail.com>
2021-10-04 01:01:26 +0000

    second commit

--- SayHi2.java
+++ SayHi2.java
@@ -2,6 +2,11 @@ public class SayHi2{
 	public static void main(String[] args)
 	{
 		System.out.println("Say Hi");
+		System.out.println("Say Hi");
+		System.out.println("Say Hi");
+		System.out.println("Say Hi");
+		System.out.println("Say Hi");
+
 	}
 
 }
